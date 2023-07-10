2596a3896725254ec2b0e383d9cc46e636871d75
a-sad-dan <danishasad91@gmail.com>
2023-07-10 16:22:20 +0530

    Added Functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const light = document.querySelector('#light-btn');
 const dark = document.querySelector('#dark-btn');
 
 const btnKeys = document.querySelectorAll('.key');
-const btnOperations = document.querySelectorAll(".operation");
+const btnOperations = document.querySelectorAll(".operation-color");
 
 
 dark.addEventListener('click', toggleDarkMode);
@@ -144,10 +144,94 @@ let timeoutId;
 theme.addEventListener('click', () => {
     clearTimeout(timeoutId);
     theme.classList.add('slide-out');
-    timeoutId = setTimeout(()=>{
+    timeoutId = setTimeout(() => {
         theme.classList.remove('slide-out');
-    },2000)
+    }, 2000)
 });
 
 // Removing the theme picker if the calculator is touched
-document.querySelector('#main-area').addEventListener('click',()=>theme.classList.remove('slide-out'));
+document.querySelector('#main-area').addEventListener('click', () => theme.classList.remove('slide-out'));
+
+
+// TOTAL NUMBER OF DIGITS = 13 digits
+
+//  LOGIC TO CALCULATE
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+function subtract(a, b) {
+    const difference = a - b;
+    return difference;
+}
+
+function product(a, b) {
+    const product = a * b;
+    return product;
+}
+
+function divide(a, b) {
+    const quotient = a / b;
+    return quotient;
+}
+
+
+// Updating the result window when button is clicked
+
+const result = document.querySelector('#result');
+const oldCalc = document.querySelector('#old-calc');
+
+const numBtnArr = document.querySelectorAll('.number');
+const OperationBtnArr = document.querySelectorAll('.operation-btn');
+
+// const buttonsArr = BtnArr.concat(operationArr);
+
+let isOperationUsed = 1;
+
+
+numBtnArr.forEach((button) => button.addEventListener('click', () => {
+    result.textContent += button.textContent;
+    isOperationUsed = 0;
+}));
+
+OperationBtnArr.forEach((button) => button.addEventListener('click', () => {
+    if (!isOperationUsed) {
+        result.textContent += button.textContent;
+        isOperationUsed = 1;
+    }
+    else {
+        result.textContent = result.textContent.slice(0, -1) + button.textContent;
+    }
+}));
+
+function clearScreen() {
+    result.textContent = '';
+    oldCalc.textContent = '';
+}
+
+// To prevent the number from overflowing
+
+
+
+// To display Result
+function displayResult() {
+    let expression = result.textContent;
+    const last = expression.slice(-1);
+    const isNotDigit = /\D/.test(last);
+    if (isNotDigit) {
+        expression = expression.slice(0, -1);
+    }
+    const value = eval(expression);
+    result.textContent = value;
+    oldCalc.textContent = expression;
+}
+
+function backspace() {
+    result.textContent = result.textContent.slice(0, -1);
+}
+result.addEventListener('click', () => {
+    result.select();
+    result.setSelectionRange(0, 9999); // For mobile devices
+    document.execCommand('copy');
+})
